653d62f4b34bf20a0db72a980787459d70f631bc
HeyKile <bellokp2002@gmail.com>
2025-01-03 12:27:55 -0500

    updated header with better formating, added buttons to image slide show, working on fade-in and fade-out of images

--- portfolio-website/src/ImageSlideShow.js
+++ portfolio-website/src/ImageSlideShow.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './ImageSlideShow.css';
 
 function ImageSlideShow({ images }) {
@@ -14,22 +15,43 @@ function ImageSlideShow({ images }) {
 
   return (
     <div className='image-slide-show-container'>
-      <div className='images-container'>
-        {images !== undefined && images.length > 0 && images[currentImageIndex]}
+      <button 
+        className='image-button-prev' 
+        onClick={() => setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length)}
+      >
+        <i className="fas fa-chevron-left"></i>
+      </button>
+      <div className='image-slide-show-container-inner'>
+        <div className='images-container'>
+          {images !== undefined && images.length > 0 && images.map((imageSrc, index) => {
+            return (
+              <img 
+                className={`app-image  ${index === currentImageIndex ? 'active' : ''}`}
+                src={imageSrc}
+              />
+            );
+          })[currentImageIndex]}
+        </div>
+        <div className='image-dots'>
+          {images !== undefined && images.length > 0 && images.map((image, index) => {
+            return (
+              <span
+                key={index}
+                className={`image-dot ${index === currentImageIndex ? 'active-dot' : ''}`}
+                onClick={() => setCurrentImageIndex(index)}
+              />
+            );
+          })}
+        </div>
       </div>
-      <div className='image-dots'>
-        {images !== undefined && images.length > 0 && images.map((image, index) => {
-          return (
-            <span
-              key={index}
-              className={`image-dot ${index === currentImageIndex ? 'active' : ''}`}
-              onClick={() => setCurrentImageIndex(index)}
-            />
-          );
-        })}
-      </div>
+      <button 
+        className='image-button-next'
+        onClick={() => setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length)}
+      >
+        <i className="fas fa-chevron-right"></i>
+      </button>
     </div>
   );
 }
 
-export default ImageSlideShow
+export default ImageSlideShow;
--- portfolio-website/src/WebsiteHeader.js
+++ portfolio-website/src/WebsiteHeader.js
@@ -2,6 +2,17 @@ import React, { useState, useEffect } from 'react';
 import ImageSlideShow from './ImageSlideShow';
 import './WebsiteHeader.css';
 
+const getImages = () => {
+  return [
+    `${process.env.PUBLIC_URL}/assets/images/kyle_grad_solo.JPEG`,
+    `${process.env.PUBLIC_URL}/assets/images/kyle_linkedin_pic.JPEG`,
+    `${process.env.PUBLIC_URL}/assets/images/kyle_grad_parents.JPEG`,
+    `${process.env.PUBLIC_URL}/assets/images/kyle_uhs_grad.JPEG`,
+    `${process.env.PUBLIC_URL}/assets/images/kyle_bush.JPEG`,
+    `${process.env.PUBLIC_URL}/assets/images/kyle_grad_sibs.JPEG`,
+  ];
+};
+
 function WebsiteHeader() {
 
   const [isShrunk, setIsShrunk] = useState(false);
@@ -10,10 +21,10 @@ function WebsiteHeader() {
   return (
     <header className="app-header">
       <div className='app-header-left-content'>
-        <h2 classname='app-header-name'>Kyle Bello</h2>
-        <p className='app-header-intro-text'>Software Enthusiast | Pitt CS Alumn | Fitness Enjoyer</p>
+        <h1 className='app-header-name'>Kyle Bello</h1>
+        <p className='app-header-intro-text'>Software Engineer | Pitt CS Alumn | Fitness Enjoyer</p>
       </div>
-      <div classname='app-header-right-conent'>
+      <div className='app-header-right-content'>
         <ImageSlideShow images={images} />
       </div>
     </header>
@@ -21,14 +32,3 @@ function WebsiteHeader() {
 }
 
 export default WebsiteHeader;
-
-const getImages = () => {
-  return [
-    <img className='app-image' src={`${process.env.PUBLIC_URL}/assets/images/kyle_grad_solo.JPEG`} alt="Me in my cap and gown holding my diploma" />,
-    <img className='app-image' src={`${process.env.PUBLIC_URL}/assets/images/kyle_linkedin_pic.JPEG`} alt="My linkedin photo" />,
-    <img className='app-image' src={`${process.env.PUBLIC_URL}/assets/images/kyle_grad_parents.JPEG`} alt="Me in my cap and gown with my parents" />,
-    <img className='app-image' src={`${process.env.PUBLIC_URL}/assets/images/kyle_uhs_grad.JPEG`} alt="Me with the other interns at my internship" />,
-    <img className='app-image' src={`${process.env.PUBLIC_URL}/assets/images/kyle_bush.JPEG`} alt="Me squatting next to a bush" />,
-    <img className='app-image' src={`${process.env.PUBLIC_URL}/assets/images/kyle_grad_sibs.JPEG`} alt="Me in my cap and gown with my siblings" />,
-  ];
-};
