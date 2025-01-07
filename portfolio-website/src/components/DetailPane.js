27a68c67299d90609c410d1a9ab797a0f14675df
Kyle Bello <112905761+HeyKile@users.noreply.github.com>
2025-01-07 11:31:51 -0500

    detail pane header styling, need to now work on body

--- portfolio-website/src/components/DetailPane.js
+++ portfolio-website/src/components/DetailPane.js
@@ -5,8 +5,21 @@ export default function DetailPane({ project, setProject }) {
 
   return (
     <div className={`detail-pane ${project !== null ? 'open' : 'close'}`}>
-      <h2>Detail Pane for {project !== null ? project.name : ''}!</h2>
-      <button onClick={() => setProject(null)}>X</button>
+      <div className='detail-pane-content'>
+        <div className='detail-pane-header'>
+          {project !== null &&
+            <div className='project-title-container'>
+              <p className='project-title'>{project.name}: <text className='project-subtext'>{project.subtext}</text></p>
+              <text className='project-technologies'>({project.technologies &&  project.technologies.join(" | ")})</text>
+            </div>
+          }
+          <button className='detail-pane-close-button' onClick={() => setProject(null)}>
+            <i className="fas fa-times"></i>
+          </button>
+        </div>
+        <div className='detail-pane-body'>
+        </div>
+      </div>
     </div>
   );
 } 
