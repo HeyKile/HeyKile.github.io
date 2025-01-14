c0a3d4f784129fc2fc0b544dbe4c3de9f1cdf2fd
HeyKile <bellokp2002@gmail.com>
2025-01-14 17:09:17 -0500

    contacts page looking decent lol, time to maybe finish this bad boy up?

--- portfolio-website/src/components/Contacts.js
+++ portfolio-website/src/components/Contacts.js
@@ -17,43 +17,41 @@ export default function Contacts({ contacts }) {
 
   return (
     <div className='contacts-container'>
-      {contacts && contacts.contactText &&
-        <p className='contact-header-text'>{contacts.contactText}</p>
-      }
-      {contacts && contacts.email && contacts.emailIcon &&
-        <div className='email-container'>
-          <div className='email-container-content'>
-            <i className={contacts.emailIcon} />
+      <div className="contacts-container-content">
+        {contacts && contacts.contactText &&
+          <h2 className='contact-header-text'>{contacts.contactText}</h2>
+        }
+        {contacts && contacts.email && contacts.emailIcon && contacts.copyIconUncopied && contacts.copyIconCopied &&
+          <div className='email-container'>
             <button
-              className='email-text'
+              className='email-info'
               onClick={() => copyToClipboard(contacts.email)}
             >
-              {contacts.email}
-              {contacts.copyIconUncopied && contacts.copyIconCopied &&
-                <i className={isCopied ? contacts.copyIconCopied : contacts.copyIconUncopied} />
-              }
+              <i className={`${contacts.emailIcon} email-icon`} />
+              <span className="email-text">{contacts.email}</span>
+              <i className={`${isCopied ? contacts.copyIconCopied : contacts.copyIconUncopied} email-icon`} />
             </button>
           </div>
+        }
+        {contacts && contacts.socialsText && 
+          <h4 className='contact-header-text'>{contacts.socialsText}</h4>
+        }
+        <div className='social-links-container'>
+          {contacts && contacts.socialLinks && contacts.socialLinks.length > 0 && contacts.socialLinks.map((contact, index) => {
+            return (
+              <a
+                className="social-content-container"
+                key={index}
+                href={contact.url}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className={`${contact.iconName}`}/>
+                <p className="social-name">{contact.name}</p>
+              </a>
+            );
+          })}
         </div>
-      }
-      {contacts && contacts.socialsText && 
-        <p className='contact-header-text'>{contacts.socialsText}</p>
-      }
-      <div className='social-links-container'>
-        {contacts && contacts.socialLinks && contacts.socialLinks.length > 0 && contacts.socialLinks.map((contact, index) => {
-          return (
-            <a
-              className="social-content-container"
-              key={index}
-              href={contact.url}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <i className={`${contact.iconName}`}/>
-              <p className="social-name">{contact.name}</p>
-            </a>
-          );
-        })}
       </div>
     </div>
   );
