87f757745bef46ca5221f35ab3e7945b88b96129
HeyKile <bellokp2002@gmail.com>
2025-01-15 13:58:37 -0500

    got work experience looking pretty damn solid if I do say so myself

--- portfolio-website/src/components/WorkExperience.js
+++ portfolio-website/src/components/WorkExperience.js
@@ -1,16 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import './WorkExperience.css';
 
 export default function WorkExperience({ workExperience }) {
 
+  const [isLoadingDescriptions, setIsLoadingDescriptions] = useState(true);
+
+  const workExperienceValid = (workExperience) => {
+    return workExperience
+      && workExperience.jobs
+      && workExperience.jobs.length > 0;
+  };
+
   useEffect(() => {
     if (workExperience && workExperience.jobs && workExperience.jobs.length > 0) {
-      workExperience.jobs.forEach((job) => {
+      setIsLoadingDescriptions(true);
+      Promise.all(workExperience.jobs.map((job) => 
         fetch(`${process.env.PUBLIC_URL}/assets/documents/${job.descriptionFileName}`)
-          .then((fileResponse) => fileResponse.text())
-          .then((text) => job.descriptionText = text)
-          .catch((error) => console.error('Error fetching job description', error));
-      });
+          .then((response) => response.text())
+          .then((text) => {
+            if (text.includes('<!DOCTYPE html>')) {
+              job.descriptionText = "";
+              console.error(`Could not find job description file for ${job.role} - ${job.companyName}`);
+            }
+            else {
+              job.descriptionText = text;
+            }
+          })
+          .catch((error) => {
+            throw Error(`Error fetching job description for ${job.role} - ${job.companyName}`, error)
+          })
+      ))
+        .catch((error) => console.error(error))
+        .finally(() => setIsLoadingDescriptions(false));
     }
   }, [workExperience]);
 
@@ -22,13 +43,50 @@ export default function WorkExperience({ workExperience }) {
         }
       </div>
       <div className="work-experience-content">
-        {workExperience && workExperience.jobs.map((job) => {
+        {workExperienceValid(workExperience) && workExperience.jobs.map((job, index) => {
           return (
-            <div className="work-experience-card">
-              {job.role && job.companyName &&
-                <div>
-                  <h3 className="work-experience-company-name">{job.role}</h3>
-                  <h5>{job.companyName}</h5>
+            <div className="work-experience-card" key={index}>
+              {job &&
+                <div className="work-experience-card-header">
+                  <div className="work-exerpience-card-header-names">
+                    {job.role &&
+                      <span className="work-experience-job-role">{job.role}</span>
+                    }
+                    {job.companyName && 
+                      <span className="work-experience-company-name">{job.companyName}</span>
+                    }
+                  </div>
+                  <div className="work-exerpience-card-header-dates">
+                    {job.startDate && job.endDate &&
+                      <span className="work-experience-job-dates">{job.startDate} - {job.endDate}</span>
+                    }
+                  </div>
+                  {/* <div className="work-exerpience-card-header-top">
+                    {job.role &&
+                      <span className="work-experience-job-role">{job.role}</span>
+                    }
+                    {job.startDate && job.endDate &&
+                      <span className="work-experience-job-dates">{job.startDate} - {job.endDate}</span>
+                    }
+                  </div>
+                  <div className="work-exerpience-card-header-bottom">
+                    {job.companyName && 
+                      <span className="work-experience-company-name">{job.companyName}</span>
+                    }
+                  </div> */}
+                </div>
+              }
+              {!isLoadingDescriptions && job && job.descriptionText && job.descriptionText.length > 0 &&
+                <div className="work-experience-card-body">
+                  <ul className="work-experience-job-description-bullet-list">
+                    {job.descriptionText.split('\n').map((text, index) => {
+                      return (
+                        <li key={index} className='work-experience-job-list-item'>
+                          <span className='work-experience-job-description-text'>{text}</span>
+                        </li>
+                      );
+                    })}
+                  </ul>
                 </div>
               }
             </div>
